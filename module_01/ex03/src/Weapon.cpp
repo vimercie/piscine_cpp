b1106e1606f2835161d661f74cad3af7f7605b7b
vimercie <vimercie@student.42lyon.fr>
2023-06-26 16:11:38 +0200

    lil fix

--- module_01/ex03/src/Weapon.cpp
+++ module_01/ex03/src/Weapon.cpp
@@ -6,13 +6,13 @@
 /*   By: vimercie <vimercie@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/26 16:02:37 by vimercie          #+#    #+#             */
-/*   Updated: 2023/06/26 16:06:50 by vimercie         ###   ########lyon.fr   */
+/*   Updated: 2023/06/26 16:09:25 by vimercie         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/Weapon.hpp"
 
-const std::string	&Zombie::getType(void)
+const std::string	&Weapon::getType(void)
 {
-	
+	return (NULL);
 }
