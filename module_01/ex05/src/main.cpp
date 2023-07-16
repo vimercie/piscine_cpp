cba04758e8858bdd181a04f8b1de0d5c3826cf0d
vimercie <vincent.mrc@hotmail.com>
2023-07-16 22:09:50 +0200

    Fixed Harl class name (Harl -> harl)

--- module_01/ex05/src/main.cpp
+++ module_01/ex05/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: vimercie <vimercie@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/15 22:11:05 by vimercie          #+#    #+#             */
-/*   Updated: 2023/07/16 21:55:03 by vimercie         ###   ########lyon.fr   */
+/*   Updated: 2023/07/16 22:09:00 by vimercie         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,12 @@
 
 int main()
 {
-	Harl    Harl;
+	Harl    harl;
 
-	Harl.complain("DEBUG");
-	Harl.complain("INFO");
-	Harl.complain("WARNING");
-	Harl.complain("ERROR");
-	Harl.complain("bruh");
+	harl.complain("DEBUG");
+	harl.complain("INFO");
+	harl.complain("WARNING");
+	harl.complain("ERROR");
+	harl.complain("bruh");
 	return (0);
 }
