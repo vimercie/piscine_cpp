1242ab8bc93c0b5f5335af3db5e7d74c1f2c3446
vimercie <vimercie@student.42lyon.fr>
2023-08-04 15:41:56 +0200

    added virtual on destructor

--- module_04/ex01/inc/Dog.hpp
+++ module_04/ex01/inc/Dog.hpp
@@ -6,7 +6,7 @@
 /*   By: vimercie <vimercie@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/30 12:16:33 by vimercie          #+#    #+#             */
-/*   Updated: 2023/07/30 17:09:07 by vimercie         ###   ########lyon.fr   */
+/*   Updated: 2023/08/04 15:41:39 by vimercie         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ class	Dog : public Animal
 	public:
 		Dog();
 		Dog(const Dog& src);
-		~Dog();
+		virtual ~Dog();
 
 		Dog&			operator=(const Dog& src);
 
