f0901f1ea3b05581611caf46dc98894f043df6ce
Kolargole <vincent.mrc@hotmail.com>
2023-08-07 17:50:29 +0200

    First commit module_05/ex01

---
+++ module_05/ex01/src/Form.cpp
@@ -0,0 +1,107 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Form.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vimercie <vimercie@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/07 16:23:13 by vimercie          #+#    #+#             */
+/*   Updated: 2023/08/07 17:48:42 by vimercie         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/Form.hpp"
+#include "../inc/Bureaucrat.hpp"
+
+Form::Form() : name("default"), isSigned(false), gradeToSign(150), gradeToExecute(150)
+{
+	std::cout << "Form default constructor called" << std::endl;
+}
+
+Form::Form(const std::string& name, int gradeToSign, int gradeToExecute) : name(name), isSigned(false), gradeToSign(gradeToSign), gradeToExecute(gradeToExecute)
+{
+	if (this->gradeToSign < 1 || this->gradeToExecute < 1)
+		throw GradeTooHighException();
+	if (this->gradeToSign > 150 || this->gradeToExecute > 150)
+		throw GradeTooLowException();
+	std::cout << "Form constructor called" << std::endl;
+}
+
+Form::Form(const Form& src) : name(src.getName()), isSigned(src.getIsSigned()), gradeToSign(src.getGradeToSign()), gradeToExecute(src.getGradeToExecute())
+{
+	std::cout << "Form copy constructor called" << std::endl;
+}
+
+Form::~Form()
+{
+	std::cout << "Form destructor called" << std::endl;
+}
+
+Form&	Form::operator=(const Form& src)
+{
+	if (this == &src)
+		return (*this);
+
+	this->isSigned = src.getIsSigned();
+
+	std::cout << "Form assignation operator called" << std::endl;
+	return (*this);
+}
+
+std::ostream&	operator<<(std::ostream& os, const Form& src)
+{
+	os << src.getName();
+	os << ", form gradeToSign " << src.getGradeToSign();
+	os << ", gradeToExecute " << src.getGradeToExecute();
+	os << ", signed: " << src.getIsSigned();
+	return (os);
+}
+
+const std::string&	Form::getName() const
+{
+	return (this->name);
+}
+
+bool	Form::getIsSigned() const
+{
+	return (this->isSigned);
+}
+
+int	Form::getGradeToSign() const
+{
+	return (this->gradeToSign);
+}
+
+int	Form::getGradeToExecute() const
+{
+	return (this->gradeToExecute);
+}
+
+void	Form::beSigned(const Bureaucrat& b)
+{
+	if (this->isSigned)
+		throw FormAlreadySignedException();
+	if (b.getGrade() > this->gradeToSign)
+		throw GradeTooLowException();
+	this->isSigned = true;
+}
+
+const char* Form::GradeTooHighException::what() const throw()
+{
+	return ("Grade too high");
+}
+
+const char* Form::GradeTooLowException::what() const throw()
+{
+	return ("Grade too low");
+}
+
+const char* Form::FormAlreadySignedException::what() const throw()
+{
+	return ("Form already signed");
+}
+
+const char* Form::FormNotSignedException::what() const throw()
+{
+	return ("Form not signed");
+}
