f56e35c8822a286cfa12842de347975ce49c4558
Kolargole <vincent.mrc@hotmail.com>
2023-08-23 18:36:53 +0200

    Handled type mismatch

--- module_07/ex00/inc/whatever.hpp
+++ module_07/ex00/inc/whatever.hpp
@@ -6,16 +6,20 @@
 /*   By: vimercie <vimercie@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/16 02:58:10 by vimercie          #+#    #+#             */
-/*   Updated: 2023/08/16 03:01:55 by vimercie         ###   ########lyon.fr   */
+/*   Updated: 2023/08/23 18:35:08 by vimercie         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef WHATEVER_HPP
 # define WHATEVER_HPP
 
+#include <typeinfo>
+
 template <typename T>
 void swap(T &a, T &b)
 {
+	if (typeid(a) != typeid(b))
+		throw std::invalid_argument("Type mismatch");
 	T tmp = a;
 	a = b;
 	b = tmp;
@@ -24,17 +28,21 @@ void swap(T &a, T &b)
 template <typename T>
 T min(T &a, T &b)
 {
+	if (typeid(a) != typeid(b))
+		throw std::invalid_argument("Type mismatch");
 	if (a < b)
 		return (a);
-	return (b);
+	return (const T& b);
 }
 
 template <typename T>
 T max(T &a, T &b)
 {
+	if (typeid(a) != typeid(b))
+		throw std::invalid_argument("Type mismatch");
 	if (a > b)
 		return (a);
-	return (b);
+	return (const T& b);
 }
 
 #endif
